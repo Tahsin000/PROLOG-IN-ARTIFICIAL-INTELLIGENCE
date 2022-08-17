01cd0e2af84c8cab92f3d04b77172500be57be7a
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-08-17 23:51:55 +0600

    Update main.pl

--- Introduction to Prolog/Nested Structure/main.pl
+++ Introduction to Prolog/Nested Structure/main.pl
@@ -1,4 +1,11 @@
+% first structure 
 has(joe,car(ford,3,5000)).
 has(joe,car,(opel,2,6000)).
 has(mick,car,(toyta,5,1000)).
 has(mick,car,(ford,2,2000)).
+
+
+% Second Structure
+likes(fued,
+  	movie(endgame,
+        	actor(chris,evens))).
